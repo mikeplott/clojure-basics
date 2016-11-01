6beeb7514a1e0c6cdab67fab4dff535bb2fe2c1e
michaelplott <mike.plott80@gmail.com>
2016-11-01 11:56:46 -0400

    turned into a web app

--- src/clojure_basics/core.clj
+++ src/clojure_basics/core.clj
@@ -1,10 +1,13 @@
 (ns clojure-basics.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 
 (def file-name "people.csv")
 
-(defn -main [& args]
+(defn read-people []
   (let [people (str/split-lines (slurp file-name))
         people (map (fn [line]
                       (str/split line #","))
@@ -13,12 +16,39 @@
         people (rest people)
         people (map (fn [line]
                       (zipmap header line))
-                 people)
-        people (filter (fn [line]
-                         (= (get line "country")
-                            "Brazil"))
-                 people)
-        people (pr-str people)]
-    (spit "people.edn" people)))
-       
+                 people)]
+    people))
+
+(defn people-html [country]
+  (let [people (read-people)
+        people (filter (fn [person]
+                         (or (nil? country)
+                             (= country (get person "country"))))
+                 people)]
+    [:ol
+     (map (fn [person]
+            [:li (str 
+                   (get person "first_name")
+                   " "
+                   (get person "last_name"))])
+       people)]))
+
+(c/defroutes app
+  (c/GET "/" []
+    (h/html [:html
+             [:body
+              (people-html)]]))
+  (c/GET "/:country" [country]
+    (h/html [:html [:body (people-html country)]])))
+
+(def server (atom nil))
+
+(defn dev []
+  (let [s (deref server)]
+    (if s
+      (.stop s)))
+  (reset! server (j/run-jetty app {:port 3000 :join? false})))
+
+(defn -main [& args]
+  (j/run-jetty app {:port 3000 :join? false}))
   
